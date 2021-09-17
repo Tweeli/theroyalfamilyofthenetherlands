f1426e7246ab35f66a29e3fd24426b2c2cfeceae
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-09-17 22:35:43 +0200

    Update warnings.js

--- commands/warnings.js
+++ commands/warnings.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const warns = JSON.parse(fs.readFileSync("./data/warns.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
-    
+
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("sorry jij kan dit niet");
 
     if (!args[0]) return message.reply("Geen gebruiker opgegeven.");
@@ -11,8 +11,6 @@ module.exports.run = async (bot, message, args) => {
     var warnUser = message.guild.member(message.mentions.users.first() || bot.users.cache.get(args[0]));
 
     if (!warnUser) return message.reply("Kan de gebruiker niet vinden.");
-
-    warns[warnUser.id].warns++;
 
     fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
         if (err) console.log(err);
